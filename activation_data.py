fda7843899011b365666f4b57ad31258427b3a14
root <root@dgx09.phx15.castle.fm>
2025-04-01 21:40:24 +0000

    ran sweep

--- activation_data.py
+++ activation_data.py
@@ -3,6 +3,7 @@ import os
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.distributed import DistributedSampler
+import h5py
 
 class ActivationDataset(Dataset):
     def __init__(self, data_root, layer_num, train=True):
@@ -13,7 +14,7 @@ class ActivationDataset(Dataset):
 
         self.files = self.files # debugging
 
-        self.idx_map = {}
+        self.idx_map = {} # TODO - cache this map so we don't have to do this slow preprocessing every time
         total_len = 0
         for fp in tqdm(self.files, desc='Preprocessing activations'):
         # for fp in self.files:
@@ -37,12 +38,40 @@ class ActivationDataset(Dataset):
             return 0
         return max(self.idx_map.keys()) + 1
 
+class HDF5ActivationDataset(Dataset):
+    def __init__(self, hdf5_file, split="train"):
+        """
+        PyTorch Dataset for HDF5 storage.
+        
+        Args:
+            hdf5_file (str): Path to the HDF5 file.
+            split (str): Dataset split, "train" or "test".
+        """
+        self.hdf5_file = hdf5_file
+        self.split = split
 
-def get_data_loaders(activation_dir, layer_num, batch_size=8, num_workers=4):
-    train_set = ActivationDataset(activation_dir, layer_num, train=True)
-    validation_set = ActivationDataset(activation_dir, layer_num, train=False)
+        # Open file to get dataset size
+        with h5py.File(self.hdf5_file, "r") as f:
+            self.length = len(f[self.split])
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index):
+        with h5py.File(self.hdf5_file, "r") as f:
+            tensor = torch.tensor(f[self.split][index])  # Load tensor lazily
+        return tensor
+
+def get_data_loaders(activation_dir, layer_num, batch_size=40, num_workers=4):
+    # train_set = ActivationDataset(activation_dir, layer_num, train=True)
+    # validation_set = ActivationDataset(activation_dir, layer_num, train=False)
+    train_set = HDF5ActivationDataset(activation_dir, split='train')
+    validation_set = HDF5ActivationDataset(activation_dir, split='validation')
+
     train_sampler = DistributedSampler(train_set, shuffle=True)
     validation_sampler = DistributedSampler(validation_set, shuffle=False)
+
     train_loader = DataLoader(train_set, batch_size, num_workers=num_workers, sampler=train_sampler, prefetch_factor=4, pin_memory=True)
     validation_loader = DataLoader(validation_set, batch_size, num_workers=num_workers, sampler=validation_sampler, prefetch_factor=4, pin_memory=True)
+
     return train_loader, validation_loader
--- train_sae.py
+++ train_sae.py
@@ -14,7 +14,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--output_dir', type=str, default='sae_out/')
 parser.add_argument('--layer_num', type=int, default=16)
 parser.add_argument('--num_epochs', type=int, default=10)
-parser.add_argument('--activation_dir', type=str, default='activations/')
+parser.add_argument('--lr', type=float, default=1e-3)
+parser.add_argument('--activation_file', type=str, default='/persist/adelworth/sae-fun/activations.h5')
+parser.add_argument('--hidden_dim_multiplier', type=int, default=4)
+parser.add_argument('--l1_lambda', type=float, default=5.0)
 args = parser.parse_args()
 
 # Initialize distributed training
@@ -26,7 +29,7 @@ def setup_ddp():
 
 # Load model and move to GPU
 def load_model(local_rank):
-    model = SAE(input_dim=4096, hidden_dim=4096 * 4).to(torch.bfloat16)
+    model = SAE(input_dim=4096, hidden_dim=4096 * args.hidden_dim_multiplier).to(torch.bfloat16)
     model = model.to(local_rank)
     model = DDP(model, device_ids=[local_rank])
     if local_rank == 0:
@@ -63,29 +66,32 @@ def validate(model, validation_loader, local_rank, writer, epoch_num):
     if local_rank == 0:
         writer.add_scalar('Loss/val_mse', avg_val_mse, epoch_num)
         writer.add_scalar('Loss/val_l1', avg_val_l1, epoch_num)
-        writer.add_scalar('Loss/val_total', avg_val_mse + model.module.l1_lambda * avg_val_l1, epoch_num)
+        writer.add_scalar('Loss/val_total', avg_val_mse + args.l1_lambda * avg_val_l1, epoch_num)
     
     return avg_val_mse, avg_val_l1
 
 def train(model, local_rank):
-    train_loader, validation_loader = get_data_loaders(args.activation_dir, args.layer_num, batch_size=64)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-    
-    # Setup TensorBoard only on main process
-    if local_rank == 0:
-        current_time = datetime.now().strftime('%b%d_%H-%M-%S')
-        writer = SummaryWriter(f'{args.output_dir}/sae_{current_time}')
+    train_loader, validation_loader = get_data_loaders(args.activation_file, args.layer_num, batch_size=64)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
     print(f'Starting training on device {local_rank}...')
 
     # Setup TensorBoard only on main process
     if local_rank == 0:
         current_time = datetime.now().strftime('%b%d_%H-%M-%S')
-        writer = SummaryWriter(f'{args.output_dir}/sae_{current_time}')
+        writer = SummaryWriter(f'{args.output_dir}/{current_time}_lambda_{args.l1_lambda}_hidden_mult_{args.hidden_dim_multiplier}_layernum_{args.layer_num}_lr_{args.lr}')
+    else:
+        writer = None
+
+    # Run validation
+    avg_val_mse, avg_val_l1 = validate(model, validation_loader, local_rank, writer, 0)
     
-    dist.barrier()
+    # Print epoch summary only on main process
+    if local_rank == 0:
+        print(f'Epoch 0: Val MSE: {avg_val_mse:.4f}, Val L1: {avg_val_l1:.4f}')
     
     for epoch_num in range(args.num_epochs):
+        dist.barrier()
         model.train()
         train_mse_loss = torch.tensor(0.0, device=local_rank)
         train_l1_loss = torch.tensor(0.0, device=local_rank)
@@ -101,7 +107,7 @@ def train(model, local_rank):
             reconstruction, sparse_representation = model(batch)
             mse_loss = torch.nn.functional.mse_loss(reconstruction, batch)
             l1_loss = torch.nn.functional.l1_loss(sparse_representation, torch.zeros_like(sparse_representation))
-            loss = mse_loss + model.module.l1_lambda * l1_loss
+            loss = mse_loss + args.l1_lambda * l1_loss
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
@@ -126,7 +132,7 @@ def train(model, local_rank):
                 if local_rank == 0:
                     writer.add_scalar('Loss/step_mse', avg_step_mse, epoch_num * len(train_loader) + step)
                     writer.add_scalar('Loss/step_l1', avg_step_l1, epoch_num * len(train_loader) + step)
-                    writer.add_scalar('Loss/step_total', avg_step_mse + model.module.l1_lambda * avg_step_l1, epoch_num * len(train_loader) + step)
+                    writer.add_scalar('Loss/step_total', avg_step_mse + args.l1_lambda * avg_step_l1, epoch_num * len(train_loader) + step)
                 
                 # Reset step accumulators
                 step_mse_loss = torch.tensor(0.0, device=local_rank)
@@ -151,7 +157,7 @@ def train(model, local_rank):
         if local_rank == 0:
             writer.add_scalar('Loss/train_mse', avg_train_mse, epoch_num)
             writer.add_scalar('Loss/train_l1', avg_train_l1, epoch_num)
-            writer.add_scalar('Loss/train_total', avg_train_mse + model.module.l1_lambda * avg_train_l1, epoch_num)
+            writer.add_scalar('Loss/train_total', avg_train_mse + args.l1_lambda * avg_train_l1, epoch_num)
         
         # Run validation
         avg_val_mse, avg_val_l1 = validate(model, validation_loader, local_rank, writer, epoch_num + 1)
@@ -163,6 +169,9 @@ def train(model, local_rank):
     
     if local_rank == 0:
         writer.close()
+    
+    if local_rank == 0:
+        torch.save(model.module.state_dict(), f'{args.output_dir}/{current_time}_lambda_{args.l1_lambda}_hidden_mult_{args.hidden_dim_multiplier}_layernum_{args.layer_num}_lr_{args.lr}/final_model.pth')
 
 def main():
     local_rank = setup_ddp()
